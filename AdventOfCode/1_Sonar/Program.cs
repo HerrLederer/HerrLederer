e9ecd655e3b9cf90af168aac180ba05d2f48da48
Christian Lederer <christian.lederer@methodpark.de>
2021-12-01 13:00:18 +0100

    Beste Lösung für 1. Dezember

---
+++ AdventOfCode/1_Sonar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Sonar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoMagicStuff();
+        }
+
+        private static void DoMagicStuff()
+        {
+            int[] fileContent = File.ReadAllLines(@"..\..\input.txt").Select(int.Parse).ToArray(); ;
+            
+            int count = 0;
+
+            int length = fileContent.Length;
+
+            for (int i = 0; i < length;)
+            {
+                int value = fileContent[i];
+                if (++i < length)
+                {
+                    int nextValue = fileContent[i];
+                    if (nextValue > value)
+                        count++;
+                }
+            }
+            Console.WriteLine("Ergebnis: " + count);
+        }
+    }
+}
