592b9025211f6e7e8ab5fcab5cad9628d6e8158a
Christian Lederer <christian.lederer@methodpark.de>
2021-12-01 13:37:06 +0100

    Sehr gute Lösung für Task 2

--- AdventOfCode/1_Sonar/Program.cs
+++ AdventOfCode/1_Sonar/Program.cs
@@ -12,27 +12,64 @@ namespace _1_Sonar
         static void Main(string[] args)
         {
             DoMagicStuff();
+            DoSmoothedMagicStuff();
         }
 
         private static void DoMagicStuff()
         {
-            int[] fileContent = File.ReadAllLines(@"..\..\input.txt").Select(int.Parse).ToArray(); ;
-            
-            int count = 0;
+            int[] fileContent = ReadFileContent();
+            int count = Count(fileContent);
+            PrintResult(count);
+        }
 
-            int length = fileContent.Length;
+        private static void DoSmoothedMagicStuff()
+        {
+            int[] fileContent = ReadFileContent();
+            int[] smoothedFileContent = Smooth(fileContent);
+            int count = Count(smoothedFileContent);
+            PrintResult(count);
+        }
+
+        private static int Count(int [] input)
+        {
+            int count = 0;
+            int length = input.Length;
 
             for (int i = 0; i < length;)
             {
-                int value = fileContent[i];
+                int value = input[i];
                 if (++i < length)
                 {
-                    int nextValue = fileContent[i];
+                    int nextValue = input[i];
                     if (nextValue > value)
                         count++;
                 }
             }
+            return count;
+        }
+
+        private static int[] Smooth(int[] input)
+        {
+            int[] fileContent = ReadFileContent();
+            IList<int> smoothedFileContent = new List<int>();
+
+            for (int i = 1; i < fileContent.Length - 1; i++)
+            {
+                int value = fileContent[i] + fileContent[i - 1] + fileContent[i + 1];
+                smoothedFileContent.Add(value);
+            }
+            return smoothedFileContent.ToArray();
+        }
+
+        private static int[] ReadFileContent()
+        {
+            return File.ReadAllLines(@"..\..\input.txt").Select(int.Parse).ToArray();
+        }
+
+        private static void PrintResult(int count)
+        {
             Console.WriteLine("Ergebnis: " + count);
         }
+
     }
 }
