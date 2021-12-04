69632abd08567797c573d4bc80adfa6daf856932
Christian Lederer <christian.lederer@methodpark.de>
2021-12-04 23:13:14 +0100

    Day 05: Preparations for a C# project

---
+++ AdventOfCode/05_/InputFileReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05_
+{
+    public class InputFileReader
+    {
+        public InputFileReader(string fileName)
+        {
+            var fileContent = File.ReadAllLines(fileName);
+        }
+    }
+}
