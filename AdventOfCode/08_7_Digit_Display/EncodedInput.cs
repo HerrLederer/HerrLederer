0e0d212b3a77f61464bd8771ffad77461d218ff3
Christian Lederer <christian.lederer@methodpark.de>
2021-12-08 22:38:07 +0100

    Day 08: Beautiful solution for Task 1

---
+++ AdventOfCode/08_7_Digit_Display/EncodedInput.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _08_7_Digit_Display
+{
+    class EncodedInput
+    {
+        public EncodedInput(string[] encoding, string[] payload)
+        {
+            Encoding = encoding;
+            Payload = payload;
+        }
+
+        public override string ToString()
+        {
+            return Serialize(Encoding) + "| " + Serialize(Payload);
+        }
+
+        private static string Serialize(string[] input)
+        {
+            string output = "";
+            foreach (string pattern in input)
+            {
+                output += pattern + " ";
+            }
+            return output;
+        }
+
+        public static List<EncodedInput> CreateEncodedInputFromFile(string fileName)
+        {
+            List<EncodedInput> ret = new List<EncodedInput>();
+
+            string[] fileContent = File.ReadAllLines(fileName);
+
+            foreach (var line in fileContent)
+            {
+                int positionOfDelimiter = line.IndexOf('|');
+                string encodingPart = line.Substring(0, positionOfDelimiter - 1);
+                string trailingEnd = line.Substring(positionOfDelimiter + 1);
+                string[] trailingEndArray = ConvertToSorted(trailingEnd.Trim().Split(' '));
+                string[] encodingPartArray = ConvertToSorted(encodingPart.Trim().Split(' '));
+
+                ret.Add(new EncodedInput(encodingPartArray, trailingEndArray));
+            }
+            return ret;
+        }
+
+        public string[] Encoding;
+        public string[] Payload;
+
+        public static string[] ConvertToSorted(string[] input)
+        {
+            string[] sortedOutput = { };
+            foreach (string pattern in input)
+            {
+                string sortedPattern = SortPattern(pattern);
+                sortedOutput = sortedOutput.Append(sortedPattern).ToArray();
+            }
+            return sortedOutput;
+        }
+
+        public static string SortPattern(string pattern)
+        {
+            char[] array = pattern.ToCharArray();
+            Array.Sort(array);
+            return new string(array);
+        }
+
+        public int Count1478()
+        {
+            return Payload.Where(i => decode(i) != -1).Count();
+        }
+
+        private static int decode(string encoding)
+        {
+            if (encoding.Length == 2)
+                return 1;
+            if (encoding.Length == 3)
+                return 7;
+            if (encoding.Length == 4)
+                return 4;
+            if (encoding.Length == 7)
+                return 8;
+
+            Console.WriteLine("Could not decode encoding '" + encoding + "'");
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode/08_7_Digit_Display/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08_7_Digit_Display
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<EncodedInput> input = EncodedInput.CreateEncodedInputFromFile(@"../../input.txt");
+            Console.WriteLine("Count1478: " + Count1478(input));
+            KeepConsoleOpen();
+        }
+
+        static int Count1478(List<EncodedInput> input)
+        {
+            int count = 0;
+            input.ForEach(i => count += i.Count1478());
+            return count;
+        }
+
+        private static void KeepConsoleOpen()
+        {
+            Console.WriteLine("Hit any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
